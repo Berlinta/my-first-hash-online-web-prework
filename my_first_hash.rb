ca5943f82af4abbda0198b3adaa5d277a9130e48
Nash Brancic <nashfft@gmail.com>
2019-02-02 00:29:57 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -25,7 +25,9 @@ def adding
                   "oil paintings" => 3
                  }
    shipping_manifest["muskets"] = 2,
-   shipping_manifest["gun powder"] = 
+   shipping_manifest["gun powder"] = 4
+   
+   puts shipping_manifest
 
   # return the shipping_manifest hash below
 
